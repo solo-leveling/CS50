93d45aff851a484364d7c6cb12f528de6c3dd28d
Hein Htet <littleshadow.mdy@gmail.com>
2024-05-23 00:02:05 +0900

    Getting request by using render

--- django_lecture1/hello/views.py
+++ django_lecture1/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def hein(request):
     return HttpResponse("Hello, Hein")
@@ -11,5 +11,14 @@ def hein(request):
 def hein(request):
     return HttpResponse("Hello, Htet")
 
+# def greet(request, name):
+#     # to get name capitalize use .capitalize
+#     return HttpResponse(f"Hello, {name.capitalize()}")
+
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}")
+    return render(request, "hello/greet.html", {
+        "name" : name.capitalize()
+    })
+
+
+    
