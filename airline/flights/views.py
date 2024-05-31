47e4652a7970c47c0a718dedeb29e81bcd6d4d8d
Hein Htet <littleshadow.mdy@gmail.com>
2024-05-31 11:22:45 +0900

    Learning SQL, Mirgration and Models

---
+++ airline/flights/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from flights.models import *
+
+# Create your views here.
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights" : Flight.objects.all()
+    })
